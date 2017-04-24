bf09761f9acf9f46d3b1432c1281bdcade18bad9
jurobinsond <jurobinson@claflin.edu>
2017-04-24 09:50:44 -0400

    Created by Coding Ground Client

---
+++ New Project-20170424/Redbox.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Redbox{
+
+     public static void main(String []args){
+        System.out.println("Welcome to Redbox\nAre you renting or returning?\nIf renting type renting\nIf returning type returning");
+        Scanner user_input = new Scanner (System.in);
+        String S1;
+        S1 = user_input.next();
+        if(user_input==renting) {
+            System.out.println("okay");
+        }
+            
+        
+     
+}
